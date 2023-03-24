4658870730cb46d6d053a598a0290908aafced14
Matin Mousavi <msvmatin5049@gmail.com>
2023-03-24 21:51:51 +0430

    updated db.json file

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import './Search.css'
+import React from "react";
+import "./Search.css";
 
 export default function Search() {
   return (
-    <section className='search-section'>
-        <p className='search-note'>Tip: You can find songs by searching the lyrics in English or فارسی</p>
+    <section className="search-section">
+      <p className="search-note">
+        Tip: You can find songs by searching the lyrics in English or فارسی
+      </p>
     </section>
-  )
+  );
 }

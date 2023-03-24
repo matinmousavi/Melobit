6150fdb97649a6272bbb7ed2931c630faa6e64b4
Matin Mousavi <msvmatin5049@gmail.com>
2023-03-24 22:00:36 +0430

    update routes

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,11 @@ import History from './Components/History/History'
 import Search from './Components/Search/Search'
 
 const routes = [
-    {path: '/' , element: <Home />},
-    {path: '/favorites' , element: <Favorites />},
-    {path: '/playlists' , element: <Playlists />},
-    {path: '/history' , element: <History />},
-    {path: '/search' , element: <Search />},
+    {path: '/Melobit' , element: <Home />},
+    {path: '/Melobit/favorites' , element: <Favorites />},
+    {path: '/Melobit/playlists' , element: <Playlists />},
+    {path: '/Melobit/history' , element: <History />},
+    {path: '/Melobit/search' , element: <Search />},
 ]
 
 export {routes}
